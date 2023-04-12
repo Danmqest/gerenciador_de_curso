2a2fdd838de67c172d415afd57af24532b53378d
Genivaldo Alves <99696430+Genivaldo2230@users.noreply.github.com>
2023-04-12 20:07:03 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input[type="text"]');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const query = input.value;
-  window.location.href = `/search?q=${query}`;
-});
