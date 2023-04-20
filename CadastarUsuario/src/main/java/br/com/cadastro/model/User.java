dedb1f8f8fe028e101497ba1807584227353079f
Lucas Bandeira <lucasbandeirai40@gmail.com>
2023-04-20 16:12:29 -0300

    Deletando os Metodos Sets e adicionando dois construtores

--- CadastarUsuario/src/main/java/br/com/cadastro/model/User.java
+++ CadastarUsuario/src/main/java/br/com/cadastro/model/User.java
@@ -5,19 +5,22 @@ public class User {
     private String name;
     private String password;
 
+    public User(String name){
+        this.name = name;
+
+    }
+    public User(String password, String name) {
+        this.password = password;
+        this.name = name;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+
 }
