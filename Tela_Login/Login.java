50052b2e7b20c26131d9660f489b5345efc66b52
Danilo Maximiano <99772416+Danmqest@users.noreply.github.com>
2023-04-26 19:41:58 -0300

    Add files via upload

---
+++ Tela_Login/Login.java
@@ -0,0 +1,14 @@
+package br.com.carstore.model;
+
+public class Login {
+    private String login;
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
