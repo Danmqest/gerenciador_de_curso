464c532b56a20392d4115eb258e5c6ee532acb49
DaniloMaximiano <danilo.profile@hotmail.com>
2023-04-26 20:28:57 -0300

    Criando o construtor

--- Tela_Login/Login.java
+++ Tela_Login/Login.java
@@ -4,6 +4,11 @@ public class Login {
     private String login;
     private String senha;
 
+    public Login(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
     public String getLogin() {
         return login;
     }
