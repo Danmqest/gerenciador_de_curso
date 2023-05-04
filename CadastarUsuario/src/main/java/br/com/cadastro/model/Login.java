7ef1befda5fa0a53ef0bfb3e53c8442daa7d5674
LucasBandeiraSilva <lucasbandeirai40@gmail.com>
2023-05-03 21:47:01 -0300

    Classe model de login

---
+++ CadastarUsuario/src/main/java/br/com/cadastro/model/Login.java
@@ -0,0 +1,19 @@
+package br.com.cadastro.model;
+
+public class Login {
+    private String login;
+    private String senha;
+
+    public Login(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
