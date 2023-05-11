fe484a4283e2c963221f0ea3b44708f9b8495297
DaniloMaximiano <danilo.profile@hotmail.com>
2023-05-10 21:19:43 -0300

    criando a class

---
+++ CadastarUsuario/src/main/java/br/com/cadastro/servlet/LoginServlet.java
@@ -0,0 +1,11 @@
+package br.com.cadastro.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+
+
+
+}
