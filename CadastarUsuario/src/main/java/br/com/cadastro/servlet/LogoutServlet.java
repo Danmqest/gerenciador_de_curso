fb06712a4ce564ebc0ea0e776499c6a17f2da507
DaniloMaximiano <danilo.profile@hotmail.com>
2023-05-17 14:02:28 -0300

    Criando

--- CadastarUsuario/src/main/java/br/com/cadastro/servlet/LogoutServlet.java
+++ CadastarUsuario/src/main/java/br/com/cadastro/servlet/LogoutServlet.java
@@ -1,4 +1,20 @@
 package br.com.cadastro.servlet;
 
-public class LogoutServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getSession().invalidate();
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+
+    }
 }
