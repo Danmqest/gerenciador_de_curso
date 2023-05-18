234a65331454b8cdb0cbe26d08e3b5a91376a13b
Amaurilio Alves da Silva <amaurilio60@gmail.com>
2023-05-18 19:43:43 -0300

    sidebar na pagina atualizada

---
+++ paginaUsuario/menu.js
@@ -0,0 +1,19 @@
+var menuItem = document.querySelectorAll('.item-menu-lateral')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+var btnExp = document.querySelector('#btn-exp')
+var menuLateral = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuLateral.classList.toggle('expandir')
+})
